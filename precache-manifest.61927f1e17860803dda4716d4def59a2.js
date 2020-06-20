d36dc165fd393073156973c899e0c27e2ef47e5f
alok-shete <shetealok@gmail.com>
2020-06-20 07:56:30 +0530

    Updates

---
+++ precache-manifest.61927f1e17860803dda4716d4def59a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23012ff72df33805f40c19e131736bf0",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "5cdbf3509916d6a24eb8",
+    "url": "/covid19/static/css/main.e2f8baa9.chunk.css"
+  },
+  {
+    "revision": "4b7280629cc8befb0840",
+    "url": "/covid19/static/js/2.fcfa65c3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/covid19/static/js/2.fcfa65c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cdbf3509916d6a24eb8",
+    "url": "/covid19/static/js/main.301ac48e.chunk.js"
+  },
+  {
+    "revision": "34491f16cdd8cac9985a",
+    "url": "/covid19/static/js/runtime-main.fa1deedf.js"
+  }
+]);
